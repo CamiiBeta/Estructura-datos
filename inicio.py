d742842ed2b3d1fd57e497248db91ead48e4b6bf
camilabema1 <camilabema1@gmail.com>
2024-08-10 17:24:22 +0000

    Ejercicio en clase

---
+++ inicio.py
@@ -0,0 +1,7 @@
+# crear una condicion donde los menores de 18 no puedan entrar
+edad = int(input("Introduce tu edad: "))
+if edad < 18:
+    print("no puedes entrar")
+else:
+    print("puedes entrar")
+    
